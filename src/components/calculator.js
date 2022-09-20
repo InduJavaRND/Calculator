ce57c34cb190b220e8925951b746d0cf3b26dc89
InduJavaRND <indumathy.javarndcorp@gmail.com>
2022-09-20 16:23:55 -0400

    Created the components

---
+++ src/components/calculator.js
@@ -0,0 +1,13 @@
+// Import React 
+import React from 'react';
+  
+// Create a Class Component Calculator.
+class Calculator extends React.Component {
+    render()
+    {
+        return "Calculator";
+    }
+}
+  
+// Export Calculator.
+export default Calculator;
